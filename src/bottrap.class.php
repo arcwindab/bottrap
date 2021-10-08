6842d7c28807a95507d8882b29ae06b879122270
ArcWind AB <91745686+arcwindab@users.noreply.github.com>
2021-10-08 23:15:16 +0200

    Update and rename function.php to bottrap.class.php

---
+++ src/bottrap.class.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @package arcwindab/bottrap
+ * @link    https://github.com/arcwindab/bottrap/
+ * @author  Tobias Jonson <suport@bot.arcwind.se>
+ * @license https://github.com/arcwindab/bottrap/blob/main/LICENSE
+ */
+
+namespace arcwindab {
+   class bottrap {
+     
+   }
+}
