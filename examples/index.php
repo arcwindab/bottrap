e8890083c2966107454abda5275d308aedc03edb
ArcWind AB <91745686+arcwindab@users.noreply.github.com>
2021-10-14 12:44:14 +0200

    Update index.php

--- examples/index.php
+++ examples/index.php
@@ -1,23 +1,23 @@
 <?php include_once __DIR__.'/../src/bottrap.class.php';
 
-$botrap = new arcwindab\bottrap();
-echo $botrap->email('test+prof@bot.example.com').'<hr>';
+$bottrap = new arcwindab\bottrap();
+echo $bottrap->email('test+prof@bot.example.com').'<hr>';
 
-$botrap->set_obfuscate(false);
-echo $botrap->email('test+prof@bot.example.com');
+$bottrap->set_obfuscate(false);
+echo $bottrap->email('test+prof@bot.example.com');
 echo '<hr>';
 
-$botrap->set_obfuscate(true);
+$bottrap->set_obfuscate(true);
 
-$botrap->set_international(false);
-echo $botrap->phone('+46 18 474 50 32');
-$botrap->set_international(true);
+$bottrap->set_international(false);
+echo $bottrap->phone('+46 18 474 50 32');
+$bottrap->set_international(true);
 echo '<br>';
-echo $botrap->phone('+46 18 474 50 32');
+echo $bottrap->phone('+46 18 474 50 32');
 echo '<hr>';
 
-$botrap->set_international(false);
-echo $botrap->phone('+46 77 114 14 00');
-$botrap->set_international(true);
+$bottrap->set_international(false);
+echo $bottrap->phone('+46 77 114 14 00');
+$bottrap->set_international(true);
 echo '<br>';
-echo $botrap->phone('+46 77 114 14 00');
+echo $bottrap->phone('+46 77 114 14 00');
